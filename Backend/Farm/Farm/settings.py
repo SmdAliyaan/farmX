5cfef7118585b223e0e0f5f32bd73ea267e2380f
Kaashif <Kaashifameen32@gmail.com>
2024-12-23 01:47:09 +0530

    email

--- Backend/Farm/Farm/settings.py
+++ Backend/Farm/Farm/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from django.core.mail import send_mail
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,7 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 mail = os.environ.get("MAIL")
 mail_pass = os.environ.get("MAIL_PASS")
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
@@ -147,3 +148,5 @@ DEFAULT_FROM_EMAIL = os.environ.get("MAIL")
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
+
+
