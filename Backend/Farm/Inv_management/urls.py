d17d4c55073682099a5f7444fd63dbcca947a1d4
Hameedsabdulkhan <hk0657708@gmail.com>
2024-12-27 23:10:49 +0530

    edit button

--- Backend/Farm/Inv_management/urls.py
+++ Backend/Farm/Inv_management/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.inventory_report, name='inventory_report'),
     path('add/', views.add_product, name='add_product'),
     path('delete/<int:pk>/', views.delete_product, name='delete_product'),
-]
+     path('edit/<int:pk>/', views.edit_product, name='edit_product'),
+     ]
